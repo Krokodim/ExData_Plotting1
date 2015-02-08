9c6be9cc62da31b20596be473a0f67ce6dba87e7
Krokodim <dmitry.grekov@gmail.com>
2015-02-08 21:37:22 +0300

    final amendments

--- plot4.R
+++ plot4.R
@@ -15,7 +15,7 @@ plot4 <- function () {
   
   # adjust the graphic parameters
   prev.par <- par(
-    bg="transparent", # as it is in the 'figure' folder
+    bg="white", # as it is in the 'figure' folder
     mfrow=c(2,2)      # 2x2 set of plots
   )
   
